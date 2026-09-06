0d698488a834f7dae3e17e456d13e41bbe61059e
Frunco <azulrio906top@gmail.com>
2026-09-05 20:21:49 -0700

    next.config remove standalone

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // output: "standalone",
   poweredByHeader: false,
   images: {
     formats: ["image/avif", "image/webp"],
